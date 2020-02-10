a240228021101d04bcd77934defda87b787c7e84
Bernadette Davis <bsd231@nyu.edu>
2020-02-10 00:31:42 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -2,17 +2,14 @@ import React, { Component } from 'react';
 
 export default class EyesOnMe extends Component {
   
-  
-  
   focusHandler = () => {
     console.log('Good!');
-    
   }
   
   blurHandler = () => {
     console.log('Hey! Eyes on me!');
-    
   }
+  
   render() {
     return (
       <div><button onFocus={this.focusHandler} onBlur={this.blurHandler} /></div>
