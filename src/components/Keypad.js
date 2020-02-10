df9e9241fab6f54307ad819430fa0d1b887cea1f
Bernadette Davis <bsd231@nyu.edu>
2020-02-10 00:31:59 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,18 +2,8 @@ import React, { Component } from 'react';
 
 export default class Keypad extends Component {
   
-  constructor(props) {
-    super();
-    this.state = {
-      //color: props.value
-    }
-  }
-  
   keyUpHandler = () => {
     console.log('Entering password...');
-    this.setState({
-      //color: '#333'
-    });
   }
   
   render() {
