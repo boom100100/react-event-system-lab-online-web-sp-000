01dcd8a02fe1a8d1ab16c5d2cb846aa2912a1602
Bernadette Davis <bsd231@nyu.edu>
2020-02-10 00:28:09 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -17,7 +17,7 @@ export default class EyesOnMe extends Component {
   
   render() {
     return (
-      <div className='cell' style={{backgroundColor: this.state.color}} onClick={this.clickHandler}></div>
+      <div><button onFocus={this.focusHandler} /></div>
     )
   }
   
